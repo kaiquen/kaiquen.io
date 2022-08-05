dbfff1555088f22d8a4d87b601fc8f5a8db4461f
Kaique Nascente <kaiquenascente@gmail.com>
2022-08-05 17:28:48 -0300

    refactor experience

--- src/components/Experience/index.tsx
+++ src/components/Experience/index.tsx
@@ -1,25 +1,53 @@
-import styles from './styles.module.scss';
-import {MdVerified} from 'react-icons/md'
+import styles from "./styles.module.scss";
+import {MdVerified} from "react-icons/md"
+import { motion } from "framer-motion";
 
+
+// 1440 - 1620 - 1800 - 2128
 const Experience = () => {
+    const width = window.screen.width;
+    let skillBox = 0;
+
+   
+    (() =>  {
+        console.log(width);
+        width < 400 
+        ? skillBox = 1540
+        : width < 900 
+        ? skillBox = 1600 
+        : width < 1200 
+        ? skillBox = 1860 
+        : width < 1500 
+        ? skillBox = 2090
+        : width < 1700 
+        ? skillBox = 2135
+        : width < 1800 
+        ? skillBox = 2170
+        : skillBox = 2570
+        console.log(skillBox)
+        console.log(width - skillBox)
+    })();
+
+  
+
     return (
         <div id="experience" className={styles.experience}>
-            <div className={styles.company}>
+            <div className={styles.container}>
                 <h1 className="heading__primary">Experiências</h1>
-                <div className={styles.content}>
+                <div className={styles.company}>
                     <a href="https://www.pjmei.app/"><img src="./assets/images/pjmei.webp" alt="" /></a>
-                    <div className={styles.bio}>
+                    <div className={styles.company__bio}>
                         <h1 className="heading__secondary">
                             Estágio - Desenvolvedor Back end 
                             <small>Abril 2022 - Presente</small>
-                            content   </h1>
-                        <ul className={styles.info}>
-                            <li className={styles.item}>Desenvolvimento de API's REST</li>
-                            <li className={styles.item}>Tecnologias usadas: Node.js, Typescript, Postgres e Typeorm</li>
+                        </h1>
+                        <ul className={styles.company__info}>
+                            <li className={styles.company__item}>Desenvolvimento de API's REST</li>
+                            <li className={styles.company__item}>Tecnologias usadas: Node.js, Typescript, Postgres e Typeorm</li>
                         </ul>
                     </div>
                 </div>
-                <div className={styles.content}>
+                <div className={styles.company}>
                     <a href="https://www.ifes.edu.br/"><img src="./assets/images/ifes.png" alt="" /></a>
                     <div className={styles.bio}>
                         <h1 className="heading__secondary">
@@ -29,10 +57,20 @@ const Experience = () => {
                     </div>
                 </div>
             </div>
-            <div className={styles.skills}>
+            <div className={styles.container}>
                 <h1 className="heading__primary">Linguagens e tecnologias</h1>
-                <div className={styles.skills__box}>
-                    <ul>
+                <div className={styles.skills}>
+                    <motion.ul 
+                        className={styles.skills__box}
+                        animate={{x: (width - skillBox)}}
+                        transition={{
+                            duration: 8,
+                            repeat: Infinity,
+                            repeatType: "reverse",
+                            repeatDelay: 1,
+                            ease: "linear"                      
+                        }}>
+                        
                         <li className={styles.skills__item}>
                             <MdVerified className={styles.skills__icon}/>
                             <p>Node.js</p>
@@ -70,7 +108,7 @@ const Experience = () => {
                             <MdVerified className={styles.skills__icon}/>
                             <p>Git</p>
                         </li>
-                    </ul>
+                    </motion.ul>
                 </div>
             </div>
         </div> 
